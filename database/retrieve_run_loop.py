17c88aa371de55db86e31c02a774b60249ffad15
chris <christopher.breed@my.jcu.edu.au>
2017-11-28 16:39:45 +1000

    changes to code to run script every 5 minutes rather than every 30 seconds

--- database/retrieve_run_loop.py
+++ database/retrieve_run_loop.py
@@ -1,12 +1,15 @@
 import requests, time, json, datetime
 import pymysql
+
 run = True
+timezone_adjustment = 2
 
 previous_time = {}
 
+five_minute_intervals = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
+
 API_TOKEN = "24DB3A5F73B12DC450FAF2718D78EB1B"
 sensors = {"ambient_temp": "49C2A9TH01", "drinks_fridge": "4852F6TH01", "food_fridge": "49C013TH01"}
-
 
 connection = pymysql.connect(host="sqldb2.cbu5ypzkmdln.ap-southeast-2.rds.amazonaws.com",
                              user="user", password="%T~wm]fK", db="sensorsData",
@@ -18,19 +21,20 @@ cursor = connection.cursor()
 for sensor_code in sensors.values():
     previous_time[sensor_code] = ''
 
-
 while run:
-    current_second = datetime.datetime.now().second
+    # current_second = datetime.datetime.now().second
+    current_minute = datetime.datetime.now().minute
 
-    # print(current_second)
-    time.sleep(1)
+    print(current_minute)
 
-    if current_second == 30 or current_second == 0:
+
+    # if current_second == 0 or current_second == 30:
+    if current_minute in five_minute_intervals:
         # print(current_second)
 
-        #run retrieval code
+        # run retrieval code
         for sensor_code in sensors.values():
-            #print('https://api.connectsense.com/v1/{}/devices/{}'.format(API_TOKEN, sensor_code))
+            # print('https://api.connectsense.com/v1/{}/devices/{}'.format(API_TOKEN, sensor_code))
             request = requests.get('https://api.connectsense.com/v1/{}/devices/{}'.format(API_TOKEN, sensor_code))
 
             response, sensor_scrap = request.text.split(",\"recent_reportings")
@@ -41,30 +45,32 @@ while run:
             # print(sensor_individual_details)
             # previous_time[sensor_individual_details['serial_number']] = sensor_individual_details['last_reported_at']
 
-            # print(sensor_individual_details['last_reported_at'])
-            date, sensor_time = str(sensor_individual_details['last_reported_at']).split('T')
-            # select only hh:mm:ss
-            trimmed_time = sensor_time[0:8]
-            cleaned_reported_date = (date, trimmed_time)
-            # put back the new date and time as a tuple
-            sensor_individual_details['last_reported_at'] = cleaned_reported_date
+            server_side_reported_date = datetime.datetime.now()
+            # print(server_side_reported_date)
 
-            print(int(cleaned_reported_date[1]))
+            server_side_reported_date -= datetime.timedelta(hours=timezone_adjustment)
+
+            req_date, req_time = str(server_side_reported_date).split(" ")
+
+            # put back the new date and time as a tuple
+            sensor_individual_details['last_reported_at'] = server_side_reported_date
+
+            # print(int(cleaned_reported_date[1]))
 
             ###sql execution
             sql_execution_individual_sensor_table = 'INSERT INTO ' + sensor_code + ' (sens_serial, ' \
-                                                                                   'sens_date, sens_time, ' \
+                                                                                   'retrieved_date, retrieved_time, ' \
                                                                                    'sens_temp, sens_humid)' \
                                                                                    ' VALUE ' \
                                                                                    '("{}", "{}", "{}", "{}", "{}");' \
-                                                                                   ''.format(sensor_individual_details['serial_number'],
-                                                                                             cleaned_reported_date[0],
-                                                                                             cleaned_reported_date[1],
-                                                                                             sensor_individual_details['temperature'],
-                                                                                             sensor_individual_details['humidity'])
+                                                                                   ''.format(
+                sensor_individual_details['serial_number'],
+                req_date,
+                req_time,
+                sensor_individual_details['temperature'],
+                sensor_individual_details['humidity'])
             print(sql_execution_individual_sensor_table)
             cursor.execute(sql_execution_individual_sensor_table)
 
-
-        # print(previous_time)
-
+            # print(current_second)
+    time.sleep(60)
