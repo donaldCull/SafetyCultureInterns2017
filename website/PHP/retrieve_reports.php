ee6f73a54939eee5e578fe39846d2000b2e9e87c
donald <donaldjames.cull@my.jcu.edu.au>
2017-11-30 17:55:25 +1000

    retrieve report has SQL test output

--- website/PHP/retrieve_reports.php
+++ website/PHP/retrieve_reports.php
@@ -6,4 +6,15 @@
  * Time: 4:42 PM
  */
 // get the q parameter from URL
-$q = $_REQUEST["q"];
+//$q = $_REQUEST["q"];
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$sql = "SELECT * FROM Report WHERE pID=1";
+
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+mysqli_free_result($result);
+
+$myJSON = json_encode($row);
+echo $myJSON;
