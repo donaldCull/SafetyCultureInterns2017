e32aa17615b7cd1b92cd67c142fa88cff79276e4
Kieran Ball <kieran.ball@my.jcu.edu.au>
2017-12-01 12:12:22 +1000

    generate reports dynamicly

--- website/js/index.js
+++ website/js/index.js
@@ -1,48 +1,71 @@
 // storage for the number of reports and its data
 // this will be replaces when we get the data dynamically
-
+var report_dates = [];
+var report_data;
+var report_id = 1;
+var sensors = [];
 
 window.onload = function start() {
     // runs the following functions when the pages load
-    report_menu_creation();
-    table_population()
+
+    getting_num_reports(function() {
+        get_sensor_names();
+        report_menu_creation()
+    });
+
+    retrieveReport(function() {
+        update_table();
+    })
+
+
 };
+
+function get_sensor_names() {
+
+    for (var i = 0; i < report_dates.length; i++){
+        sensors[i] = (Object.keys(report_data)[i])
+    }
+}
+
+function getting_num_reports(callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200){
+            report_dates = JSON.parse(this.responseText);
+            callback();
+        }
+    };
+    xhttp.open("GET", "../PHP/retrieve_number_of_reports.php", true);
+    xhttp.send();
+
+
+
+}
+
 
 function report_menu_creation() {
     // populates the tab menu for the number of reports/incidents we have
     var active = "active";
-    for (var i = 0; i < reports.length; i++){
+    for (var i = 0; i < report_dates.length; i++){
         var link = "#";
-        var id = "" + reports[i];
-        var label = "" + reports[i];
+        var id = "" + report_dates[i].report_date;
+        var label = "" + report_dates[i].report_date;
         document.getElementById("reports_menu").innerHTML += "<a onclick='on_tab_click(this)' href=\"" + link + "\" class=\"list-group-item list-group-item-action "+ active +"\" id=\"" + id + "\">" + label + "</a>";
         active = "";
     }
 }
 
-function table_population() {
-    // populates the table with the data in values
-    var report_table = document.getElementById("report_table");
-    var values_count = 0;
-    for (var x = 1; x < 8; x++){
-    for(var i = 1; i < 5; i++){
-        report_table.rows[x].cells[i].innerHTML = values[values_count];
-        values_count++
-        }
-    }
-}
 
-function retrieveReport() {
+function retrieveReport(callback) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200){
-            raw_data = JSON.parse(this.responseText);
+            var raw_data = JSON.parse(this.responseText);
             report_data = JSON.parse(raw_data[0].report_json);
-            //document.getElementById("test_output").innerHTML = raw_data[0].report_json;
             callback();
         }
     };
-    xhttp.open("GET", "../PHP/retrieve_reports.php?q="+1, true);
+    xhttp.open("GET", "../PHP/retrieve_reports.php?q="+report_id, true);
     xhttp.send();
 }
 
@@ -52,16 +75,48 @@ function on_tab_click(this_object) {
     // currently changes the list button item state when clicked
     // will add functionality to get and display the correct data
 
-    var count = 1;
+
     // The count is the primary key of the report in the database
-    var id = this_object.id;
-    retrieveReport(id);
-    for (var i = 0; i < reports.length; i++){
-        var object_name = "report " + count;
+    for (var i = 0; i < report_dates.length; i++){
+        var object_name = report_dates[i].report_date;
         var object = document.getElementById(object_name);
         $(object).removeClass("active");
-        count++;
+
 
     }
     $(this_object).addClass("active");
+
+    // get the report
+    var count = 1;
+    for (i = 0; i < report_dates.length; i++){
+        if(this_object.id == report_dates[i].report_date){
+            report_id = count
+        }
+        count++;
+    }
+
+
+    retrieveReport(function() {
+        update_table();
+    })
+
+}
+
+function update_table() {
+    var report_table = document.getElementById("report_table");
+
+    var count = 0;
+    for (var i = 1; i < 5; i++){
+        report_table.rows[1].cells[i].innerHTML = report_data.Sensor_1.Monday[count];
+        report_table.rows[2].cells[i].innerHTML = report_data.Sensor_1.Tuesday[count];
+        report_table.rows[3].cells[i].innerHTML = report_data.Sensor_1.Wednsday[count];
+        report_table.rows[4].cells[i].innerHTML = report_data.Sensor_1.Thursday[count];
+        report_table.rows[5].cells[i].innerHTML = report_data.Sensor_1.Friday[count];
+        report_table.rows[6].cells[i].innerHTML = report_data.Sensor_1.Saturday[count];
+        report_table.rows[7].cells[i].innerHTML = report_data.Sensor_1.Sunday[count];
+        count++;
+    }
+
+
+
 }
