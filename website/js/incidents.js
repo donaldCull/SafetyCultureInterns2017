a5e35cd2da18c09a39f85ad5dc2b29efe64186bf
Kieran Ball <kieran.ball@my.jcu.edu.au>
2017-12-04 16:31:55 +1000

    updates

--- website/js/incidents.js
+++ website/js/incidents.js
@@ -1,34 +1,18 @@
 // storage for the IDs of reports and its data
 var raw_data;
-var report_id_list = [];
+var sensors = [];
+var unique_sensors = [];
+
 
 
 
 window.onload = function start() {
     // runs the following functions when the pages load
     get_raw_data(function () {
-        create_menu_items();
-        update_table(0);
+        sensors_menu();
     });
 };
 
-
-function create_menu_items() {
-    // creates a list of IDs to user for the side menu
-    for (var i = 0; i < raw_data.length; i++) {
-        report_id_list[i] = raw_data[i].pID;
-    }
-
-    // creates and adds the menu items
-    var active = "active";
-    for (i = 0; i < report_id_list.length; i++) {
-        var link = "#";
-        var id = "" + report_id_list[i];
-        var label = "#00" + report_id_list[i];
-        document.getElementById("reports_menu").innerHTML += "<a onclick='on_tab_click(this)' href=\"" + link + "\" class=\"list-group-item list-group-item-action " + active + "\" id=\"" + id + "\">" + label + "</a>";
-        active = "";
-    }
-}
 
 
 function get_raw_data(callback) {
@@ -44,27 +28,6 @@ function get_raw_data(callback) {
     xhttp.send();
 }
 
-
-function on_tab_click(this_object) {
-    // changes the list button item state when clicked
-    var count = 0;
-    for (var i = 0; i < report_id_list.length; i++) {
-        var object_name = report_id_list[i];
-        var object = document.getElementById(object_name);
-        $(object).removeClass("active");
-        count++;
-
-    }
-    $(this_object).addClass("active");
-
-
-    // updates incident table based on selection
-    var ob_id = this_object.id;
-    ob_id -= 1;
-    update_table(ob_id);
-}
-
-
 function update_table(ob_id) {
     // a stupid way to update the table, but it works so...
     var report_table = document.getElementById("report_table");
@@ -77,5 +40,21 @@ function update_table(ob_id) {
     report_table.rows[6].cells[1].innerHTML = raw_data[ob_id].incid_temp;
     report_table.rows[7].cells[1].innerHTML = raw_data[ob_id].incid_date_stop;
     report_table.rows[8].cells[1].innerHTML = raw_data[ob_id].incid_time_stop;
+
+}
+
+function sensors_menu() {
+
+    for (var x = 0; x < raw_data.length; x++){
+        sensors[x] = raw_data[x].incid_serial;
+    }
+
+
+    for (var i = 0; i < raw_data.length; i++){
+        var link = "#";
+        var id = raw_data[i].incid_serial + " " + raw_data[i].pID;
+        var label = "" + raw_data[i].incid_location + " - " + raw_data[i].incid_name;
+        document.getElementById("reports_menu").innerHTML += "<a onclick='on_tab_click(this)' href=\"" + link + "\" class=\"list-group-item list-group-item-action \" id=\"" + id + "\">" + label + "</a>";
+    }
 
 }
