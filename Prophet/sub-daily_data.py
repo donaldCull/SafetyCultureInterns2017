2a196b15b5ef574c195f4066c3485c4a01323a15
donald <donaldjames.cull@my.jcu.edu.au>
2017-12-05 16:13:44 +1000

    threshold line added to visualisation

--- Prophet/sub-daily_data.py
+++ Prophet/sub-daily_data.py
@@ -4,24 +4,31 @@ import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
 
-
-# Python
 df = pd.read_csv('data.csv')
-print(df.head(5), end='\n')
-print(df.dtypes, end='\n')
 # change first column to datatime
 df['ds'] = pd.DatetimeIndex(df['ds'])
-print(df.dtypes, end='\n')
 
+# make a plot of the historical data
+threshold = 5.0
 ax = df.set_index('ds').plot(figsize=(12,8))
+ax.axhline(y=threshold, color='r', linestyle='-')
 ax.set_ylabel('Temperature recordings')
 ax.set_xlabel('DateTime')
 
 model = Prophet(changepoint_prior_scale=0.01).fit(df)
+# produce 300 future timestamps in 5 minute increments
 future_times = model.make_future_dataframe(periods=300, freq='5min', include_history=False)
+# use the model to predict temps for these times
 forecast = model.predict(future_times)
+# Write the date and temp to file
+forecast.to_csv('Predict_Output', columns=('ds', 'yhat'), index_label=False, index=False, header=('Timestamp', 'Temps'), float_format='%.2f')
+# display the last 5 observations and their predicted temps with upper and lower boundaries
+print()
 print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
 
-model.plot(forecast)
+# plot the forecast results
+model.plot(forecast, uncertainty=True)
+# plot the overall and daily trend
 model.plot_components(forecast)
+
 plt.show()
