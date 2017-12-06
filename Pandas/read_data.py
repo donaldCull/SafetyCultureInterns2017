b52bf7c5a8bd10d24ff68971de76de87522e358e
donald <donaldjames.cull@my.jcu.edu.au>
2017-12-06 10:12:06 +1000

    added ideas that need to be explored to make predictions dynamic

--- Pandas/read_data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("../CSV/49C2A9TH01-Ambient-temp-altered.csv", sep=',')
-print(df)
-
-
--- Prophet/sub-daily_data.py
+++ Prophet/sub-daily_data.py
@@ -3,14 +3,17 @@ from fbprophet import Prophet
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
-
+# csv data read needs to be dynamic
 df = pd.read_csv('data.csv')
-# change first column to datatime
+# change first column to datetime
 df['ds'] = pd.DatetimeIndex(df['ds'])
 
 # make a plot of the historical data
+# threshold needs to be extracted from user preferences
 threshold = 5.0
 ax = df.set_index('ds').plot(figsize=(12,8))
+
+# Provide a way to use these graphs on the website
 # add a threshold line to historical data vis
 ax.axhline(y=threshold, color='r', linestyle='-')
 ax.set_ylabel('Temperature recordings')
@@ -21,6 +24,8 @@ model = Prophet(changepoint_prior_scale=0.01).fit(df)
 future_times = model.make_future_dataframe(periods=300, freq='5min', include_history=False)
 # use the model to predict temps for these times
 forecast = model.predict(future_times)
+
+# predictions stored in a csv file and overwritten each time? Or update a database with predictions
 # Write the date and temp to file
 forecast.to_csv('Predict_Output', columns=('ds', 'yhat'), index_label=False, index=False, header=('Timestamp', 'Temps'), float_format='%.2f')
 # display the last 5 observations and their predicted temps with upper and lower boundaries
