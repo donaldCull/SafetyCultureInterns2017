dd2739684f9413d35d2d493532af1c93fafa69bb
donald <donaldjames.cull@my.jcu.edu.au>
2017-12-06 15:16:54 +1000

    index bug fixed for read csv

--- CSV/read_csv.py
+++ CSV/read_csv.py
@@ -6,10 +6,10 @@ EMPTY = '--'
 csv_filenames = ['49C2A9TH01-as-of-20171206.csv', '49C013TH01-as-of-20171206.csv', '4852F6TH01-as-of-20171206.csv']
 altered_csv = []
 for csv_filename in csv_filenames:
-    with open(csv_filename) as csvfile:
+    with open(csv_filenames[0]) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         # Consume headers line
-        altered_csv.append(next(readCSV))
+        next(readCSV)
         for row in readCSV:
             if row[1] != EMPTY and row[2] != EMPTY:
                 # get date/time as correct format
@@ -18,19 +18,10 @@ for csv_filename in csv_filenames:
                 row[0] = altered_time
                 altered_csv.append(row)
 
-    for row in altered_csv:
-        print(len(altered_csv))
+
+    with open("altered-" + csv_filename, 'w' ) as writeFile:
+        csv_row_count = len(altered_csv)
+        writer = csv.writer(writeFile)
+        for row in range(csv_row_count - 1, -1, -1):
+            writer.writerow(altered_csv[row])
         altered_csv.clear()
-
-
-    # with open("altered-" + csv_filename, 'w' ) as writeFile:
-    #     csv_row_count = len(altered_csv)
-    #     writer = csv.writer(writeFile)
-    #     for row in range(csv_row_count - 1, 0, -1):
-    #         writer.writerow(altered_csv[row])
-    #     altered_csv.clear()
-
-        # for row in range(extracted_data_length - 1, 0, -1):
-        #     print(extracted_data[row])
-        #     writeCSV.writerow(extracted_data[row])
-
