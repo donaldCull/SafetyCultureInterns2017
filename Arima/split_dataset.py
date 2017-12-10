1b5bcabebbc1dfe3c66acb1bf937eb64e9b64f21
chris <christopher.breed@my.jcu.edu.au>
2017-12-11 09:32:06 +1000

    Delete unused files

--- Arima/split_dataset.py
+++
@@ -1,8 +0,0 @@
-# split the dataset
-from pandas import Series
-series = Series.from_csv('../Prophet/data.csv')
-split_point = len(series) - 7
-dataset, validation = series[0:split_point], series[split_point:]
-print('Dataset %d, Validation %d' % (len(dataset), len(validation)))
-dataset.to_csv('dataset.csv')
-validation.to_csv('validation.csv')
