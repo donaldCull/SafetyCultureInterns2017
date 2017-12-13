a359dd9598fc37c051b146750c47e226cf62a83e
Kieran Ball <kieran.ball@my.jcu.edu.au>
2017-12-13 15:53:28 +1000

    updates to starting website

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,14 @@
 function on_login_click() {
-    var username = document.getElementById("login_email").value;
-    var password = document.getElementById("login_password").value;
+    // var username = document.getElementById("username_input").value;
+    // var password = document.getElementById("password_input").value;
+    //
 
+    // if(true){//username === "user@TempSpace.com" && password.length >= 5){
+        console.log("Success");
+        window.location.href = 'html/reports.html';
+    // }
+    // else {
+    //     document.getElementById("error_text").style.visibility = "visible";
+    // }
 
 }
